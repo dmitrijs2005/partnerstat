8a32bfa66e5074610a639d284ea2357f23eaaa48
dima <dmitrijs2005@gmail.com>
2016-09-06 17:50:09 -0400

    Change settings

--- partnerstat/partnerstat/settings.py
+++ partnerstat/partnerstat/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(BASE_DIR), 'static'))
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'pub/static/')
 
 STATIC_URL = '/static/'
 
