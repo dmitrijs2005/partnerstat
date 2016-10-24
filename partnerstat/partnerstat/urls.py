6cda9a9e16c4228204c2d866ccf5a6405e4d1b62
dima <dmitrijs2005@gmail.com>
2016-10-24 13:02:19 -0400

    Logout fixed

--- partnerstat/partnerstat/urls.py
+++ partnerstat/partnerstat/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from stats.views import index as stats_index
 
 urlpatterns = [
     url(r'^$', stats_index),
     url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/'}),
     url(r'^stats/', include('stats.urls')),
     url(r'^admin/', admin.site.urls),
     url('^', include('django.contrib.auth.urls')),
